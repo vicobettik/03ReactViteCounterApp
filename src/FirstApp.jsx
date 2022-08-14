4d15a58b2c536a238fc70432cf635062720a2cda
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2022-08-14 13:46:11 -0500

    Prueba en el componente fistApp, configuracion correcta de proyecto con yarn

---
+++ src/FirstApp.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subTitle, name }) => {
+
+  // console.log(props);
+  
+  return (
+    <>
+      <h1 data-testid="test-title">{ title }</h1>
+      <p>{ subTitle }</p>
+      <p>{ subTitle }</p>
+      <p>{ name }</p>
+    </>
+  )
+}
+
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+}
+
+FirstApp.defaultProps = {
+  name: 'Fernando Herrera',
+  subTitle: 'No hay subtítulo',
+  // title: 'No hay título',
+}
